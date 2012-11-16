b2fd7da5286258479caf2610eb97d01eeaa9113c
Andrew Benz <andorbal@gmail.com>
2012-11-15 18:49:35 -0600

    Changed the display of whether you own the song or not

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,2 +1,15 @@
 module SongsHelper
+  def show_ownership(song)
+    if @owned_songs.any? {|owned| owned == song}
+      text = "I own it"
+      class_name = "label-success"
+    else
+      text = "I don't own it"
+    end
+
+    content_tag(:span, class: "label #{class_name}",
+                "data-url" => own_song_path(song)) do
+      text
+    end
+  end
 end
