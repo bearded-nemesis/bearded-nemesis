b842a7e17e2754ef652bc3828e9946f7cb05e2e7
Andrew Benz <andorbal@gmail.com>
2012-11-17 12:42:10 -0600

    Fixed a css bug where the web fonts would not download when using SSL

--- db/schema.rb
+++ db/schema.rb
@@ -74,7 +74,6 @@ ActiveRecord::Schema.define(:version => 20121115234718) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
-    t.string   "artist"
     t.boolean  "admin"
   end
 
