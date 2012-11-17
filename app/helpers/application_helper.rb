d119901157e5cbe1dc812dbf70c5ecd185771e0e
Andrew Benz <andorbal@gmail.com>
2012-11-17 14:02:19 -0600

    Added gravatars to the users list and details

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,14 @@ module ApplicationHelper
     puts current_user
     (current_user) ? current_user.is_admin? : false
   end
+
+  def avatar_url(user, size)
+    if user.respond_to? :avatar_url
+      user.avatar_url
+    else
+      default_url = "#{root_url}images/guest.png"
+      gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size || 48}"
+    end
+  end
 end
