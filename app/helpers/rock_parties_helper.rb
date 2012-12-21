e326df00c5060bfb05feef1978b32bcd6a505196
Nathan Englert <nenglert@gmail.com>
2012-12-20 18:52:27 -0600

    Added helper to create selected option when attendee is already part of the rock party.  Fixes #37

--- app/helpers/rock_parties_helper.rb
+++ app/helpers/rock_parties_helper.rb
@@ -1,2 +1,13 @@
 module RockPartiesHelper
+  def attendee_option(user)
+    if @rock_party.users && @rock_party.users.include?(user)
+      content_tag(:option, selected: "selected", value: user.id) do
+        user.email
+      end
+    else
+      content_tag(:option, value: user.id) do
+        user.email
+      end
+    end
+  end
 end
