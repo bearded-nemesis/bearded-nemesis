0bf17de109f0cbc21af13f0e73656342c965d672
Nathan Englert <nenglert@gmail.com>
2013-01-10 19:49:22 -0600

    Fixed global songs search

--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ BeardedNemesis::Application.routes.draw do
 
     collection do
       get :mine
+      get "search" => "songs#search"
       get "search/:term" => "songs#search"
       get "autocomplete" => "songs#autocomplete"
     end
