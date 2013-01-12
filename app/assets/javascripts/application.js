d2afadda3803161189ef5f8651d6effede57a17d
Andrew Benz <andorbal@gmail.com>
2013-01-11 19:19:01 -0600

    Added the widget jquery ui back in

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require jquery.ui.widget
 //= require jquery.ui.datepicker
 //= require bootstrap/js/bootstrap.js
 //= require bootstrap/js/bootstrap-dropdown.js
