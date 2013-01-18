61cb7258d7cd3260e0199c24b31d334a15205e0a
William Benz <william.a.benz@gmail.com>
2013-01-17 20:23:27 -0600

    Added rock party to the playlist model and created a migration for it.

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,9 +1,10 @@
 class Playlist < ActiveRecord::Base
   belongs_to :user
-  attr_accessible :amount_of_songs, :include_unrated_songs, :name, :unrated_songs_rating, :user
+  attr_accessible :amount_of_songs, :include_unrated_songs, :name, :unrated_songs_rating, :user, :rock_party
 
   has_and_belongs_to_many :users, :uniq => true
   has_many :playlist_songs
+  belongs_to :rock_party
 
   def players
     users + [user]
