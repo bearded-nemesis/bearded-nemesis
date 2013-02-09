6169fbd3cbf98d115bb2f32bf8364c7abb944b86
Andrew Benz <andorbal@gmail.com>
2013-02-08 22:01:13 -0600

    Removed the non-rspec and cucumber tests

--- test/unit/helpers/admin/whitelists_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class Admin::WhitelistsHelperTest < ActionView::TestCase
-end
--- test/unit/helpers/rock_parties_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class RockPartiesHelperTest < ActionView::TestCase
-end
