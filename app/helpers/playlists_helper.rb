ca5268cf4b9ecbdb849237139e4ca05f7223f2c9
Andrew Benz <andorbal@gmail.com>
2013-03-12 18:56:29 -0500

    Added the ability the write angular templates in HAML

--- app/helpers/playlists_helper.rb
+++ app/helpers/playlists_helper.rb
@@ -1,12 +1,12 @@
 module PlaylistsHelper
   def player_option(user)
-    user_select_option(@playlist.users, user)
+    user_select_option(@playlist.players.map{|x| x.user}, user)
   end
 
   def instrument_select_list(playlist_song, player)
-    selectedInstrument = playlist_song.instrument_for player
+    selectedInstrument = playlist_song.instrument_for player.user
 
-    select_tag "#{player.email}[#{playlist_song.song.name}]", options_for_select(ApplicationController::INSTRUMENTS_WITH_PRO, selectedInstrument),
+    select_tag "#{player.user.email}[#{playlist_song.song.name}]", options_for_select(ApplicationController::INSTRUMENTS_WITH_PRO, selectedInstrument),
                :include_blank => true, :class => "instrument-select", "data-player" => player.id, "data-url" => playlist_song_path(playlist_song)
   end
 end
